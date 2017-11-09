7946a9e1f76d4d23bfb68c2938c4e2defa939fe7
visrahane <rahane@usc.edu>
2017-11-09 12:42:04 -0800

    Add read any frame from file implementation

---
+++ src/com/vd/constants/VideoConstant.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package com.vd.constants;
+
+/**
+ * @author Vis
+ *
+ */
+public interface VideoConstant {
+	public static final int VIDEO_PLAYER_WIDTH = 352;
+	public static final int VIDEO_PLAYER_HEIGHT = 288;
+	public static final long VIDEO_FRAME_SIZE = VIDEO_PLAYER_WIDTH * VIDEO_PLAYER_HEIGHT * 3;
+	public static final int VIDEO_BUFFER_COUNT = 6000;
+
+}
